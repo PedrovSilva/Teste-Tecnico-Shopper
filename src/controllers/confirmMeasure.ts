bc3d3546e79c4ebc5b74c50153626c84732c4ecc
Pedro Vitor <82241565+PedrovSilva@users.noreply.github.com>
2024-08-28 18:09:18 +0000

    fix: pequenas mudancas

--- src/controllers/confirmMeasure.ts
+++ src/controllers/confirmMeasure.ts
@@ -4,8 +4,8 @@ import { v4 as uuidv4 } from 'uuid'; // Para gerar GUIDs (se necessário)
 import { DatabaseService } from "./databaseService"; // Suponha que você tenha um serviço de banco de dados
 
 @Controller("confirm")
-export class ConfirmMeasure
-Controller {
+export class ConfirmMeasureController
+ {
     private dbService: DatabaseService;
 
     constructor() {
