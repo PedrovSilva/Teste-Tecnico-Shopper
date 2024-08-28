d11136670aaffdb5eff094c8a374c429f02c0f8f
Pedro Vitor <82241565+PedrovSilva@users.noreply.github.com>
2024-08-28 20:01:29 +0000

    chore: add axios

--- src/clients/genai.ts
+++ src/clients/genai.ts
@@ -1,4 +1,10 @@
+import {AxiosStatic} from "axios";
+
 export class Genai {
+    constructor(protected request: AxiosStatic){
+        
+    }
+
     public async fetchMeasure(image: String): Promise<{}>{
         return Promise.resolve({});
 
